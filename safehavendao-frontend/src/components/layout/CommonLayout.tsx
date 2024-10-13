303bb41b0877dabaf2e623dc5ca7cd241a5f8ea2
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-10-13 18:26:09 +0300

    Working on the header styling

--- safehavendao-frontend/src/components/layout/CommonLayout.tsx
+++ safehavendao-frontend/src/components/layout/CommonLayout.tsx
@@ -18,12 +18,12 @@ export default function Layout({ children }: LayoutProps) {
         <Sidebar />
         <Box
           as="main"
-          mt="3em"
+          mt="2em"
           ml={isMobile ? 0 : "64px"} // No sidebar on mobile, margin for desktop
           flexGrow={1}
           p={isMobile ? 3 : 5} // Adjust padding based on screen size
           bg="#141E30"
-          color="white"
+          color="gray"
           overflow="auto"
         >
           {children}
