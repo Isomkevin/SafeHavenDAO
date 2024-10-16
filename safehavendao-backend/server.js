3b7434845281bcc2e3de9001abbd05e9e9ba78e4
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-10-16 14:08:15 +0300

    Removing CORS debugging features

--- safehavendao-backend/server.js
+++ safehavendao-backend/server.js
@@ -53,11 +53,6 @@ connect(mongoUri, {
 app.use("/ussd", ussdRoutes);
 app.get("/", (req, res) => res.send("HELLO FROM HOMEPAGE"));
 
-app.options("/products/:id", cors()); // enable pre-flight request for DELETE request
-app.delete("/products/:id", cors(), function (req, res, next) {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
-});
-
 // General error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
